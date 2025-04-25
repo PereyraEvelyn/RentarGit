d9e96da16da60ddf55764c65b1690e812623a2bb
Pereyra Evelyn <pereyra.evelyn.micaela@gmail.com>
2025-04-25 19:02:02 -0300

    Se creo la clase VehiculoElectrico y VehiculoACombustible

---
+++ app/src/main/java/rentar/VehiculoACombustion.java
@@ -0,0 +1,7 @@
+package rentar;
+
+public class VehiculoACombustion {
+    private double consumoCombustible;  
+    private double nivelEmisiones;       
+}
+
---
+++ app/src/main/java/rentar/VehiculoElectrico.java
@@ -0,0 +1,7 @@
+package rentar;
+
+public abstract class VehiculoElectrico {
+    private double autonomia;          
+    private double tiempoRecarga;      
+
+}
