ce99b0a3994945431a71ad78da989de4cd592086
Pereyra Evelyn <pereyra.evelyn.micaela@gmail.com>
2025-04-25 19:06:00 -0300

    Correccion de herencia Vehiculo

--- app/src/main/java/rentar/VehiculoACombustion.java
+++ app/src/main/java/rentar/VehiculoACombustion.java
@@ -1,6 +1,6 @@
 package rentar;
 
-public class VehiculoACombustion {
+public class VehiculoACombustion extends Vehiculo {
     private double consumoCombustible;  
     private double nivelEmisiones;       
 }
--- app/src/main/java/rentar/VehiculoElectrico.java
+++ app/src/main/java/rentar/VehiculoElectrico.java
@@ -1,6 +1,6 @@
 package rentar;
 
-public abstract class VehiculoElectrico {
+public abstract class VehiculoElectrico extends Vehiculo{
     private double autonomia;          
     private double tiempoRecarga;      
 
