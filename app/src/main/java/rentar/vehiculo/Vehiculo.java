6d34f23d65b0af1abc783bcccc592ab5fe2ad14f
EvelynMPereyra <pereyra.evelyn.micaela@gmail.com>
2025-05-16 13:24:13 -0300

    Se crearon las clases auto, suv, camioneta, furgoneta. Interfaces Electrico y ACombustion

--- app/src/main/java/rentar/vehiculo/Vehiculo.java
+++ app/src/main/java/rentar/vehiculo/Vehiculo.java
@@ -5,6 +5,36 @@ public abstract class Vehiculo {
     private String marca;
     private String modelo;
     private double kilometraje;
-    private int año;
-    private String estado;
+    private int anio;
+    private Estado estado;
+
+    public enum Estado {
+     Disponible, Rentado, EnMantenimiento;
+    }
+    
+    /*Getters 
+    public String getPatente() {
+        return patente;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public double getKilometraje() {
+        return kilometraje;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }*/
+
 }
