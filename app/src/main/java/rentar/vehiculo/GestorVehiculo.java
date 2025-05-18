c30cb8bd8fdccac22e205e814909e80c4036e1c4
Raul Asson <raul.indu.13.@gmail.com>
2025-05-18 14:48:47 -0300

    Se creo la clase GestorVehiculo con sus respectivos metodos

---
+++ app/src/main/java/rentar/vehiculo/GestorVehiculo.java
@@ -0,0 +1,41 @@
+package rentar.vehiculo;
+import java.util.ArrayList;
+
+public class GestorVehiculo {
+    private ArrayList<Vehiculo> vehiculos;
+
+    public GestorVehiculo() {
+        setVehiculos(new ArrayList<Vehiculo>());
+    }
+
+    public void setVehiculos(ArrayList<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public void agregarVehiculo(Vehiculo vehiculo){
+        this.vehiculos.add(vehiculo);
+    }
+
+    public void eliminarVehiculo(String patente){
+        Vehiculo vehiculoEncontrado = getVehiculo(patente);
+        vehiculos.remove(vehiculoEncontrado);
+    }
+
+     public Vehiculo getVehiculo(String patente){
+        Vehiculo vehiculoEncontrado = null;
+        for (Vehiculo var : vehiculos) {
+            if (var.getPatente().equals(patente)) {
+                vehiculoEncontrado = var;
+                break;
+            }
+        }   
+        return vehiculoEncontrado;
+    }
+
+    public void modificarVehiculo(Vehiculo nuevoVehiculo){
+        Vehiculo vehiculoEncontrado;
+        vehiculoEncontrado = getVehiculo(nuevoVehiculo.getPatente());
+        vehiculos.remove(vehiculoEncontrado); 
+        vehiculos.add(nuevoVehiculo);               
+	}
+}
