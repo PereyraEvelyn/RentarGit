6ae3c15315b59f7323f3b3b792f6d166c8955ea9
Raul Asson <raul.indu.13.@gmail.com>
2025-05-18 13:16:28 -0300

    se creo la clase GestorMantenimiento y sus respectivos metodos

--- app/src/main/java/rentar/mantenimiento/Mantenimiento.java
+++ app/src/main/java/rentar/mantenimiento/Mantenimiento.java
@@ -1,15 +1,34 @@
 package rentar.mantenimiento;
 import java.time.LocalDate;
+import rentar.vehiculo.*;
 
 public class Mantenimiento {    
         private LocalDate fecha;
         private String detalles_mantenimiento;
+        private Vehiculo vehiculo;
         
         
-        public Mantenimiento(LocalDate fecha, String detalles_mantenimiento) {
+        public Mantenimiento(LocalDate fecha, String detalles_mantenimiento, Vehiculo vehiculo) {
             this.fecha = fecha;
             this.detalles_mantenimiento = detalles_mantenimiento;
+            this.vehiculo = vehiculo;
         }
+
+
+        public LocalDate getFecha() {
+            return fecha;
+        }
+
+
+        public String getDetalles_mantenimiento() {
+            return detalles_mantenimiento;
+        }
+
+        
+        
+        
+        
+        
 
         
 
